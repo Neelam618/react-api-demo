8c5a0805ea4555fc742785eeb2edaa6b59ba0a7e
Neelam618 <neelamchavan410@gmail.com>
2021-05-28 15:53:21 +0530

    update api data with put method

--- react-api-demo/src/API methods/PutMethod.js
+++ react-api-demo/src/API methods/PutMethod.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react'
 
 export default function PutMethod() {
     const [data, setData] = useState([])
-    
-    const [userId, setUserId] =useState([])
-    const [title, setTitle] =useState([])
-    const [body, setBody] =useState([])
+
+    const [userId, setUserId] = useState("")
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+    const [id, setId] = useState(null)
+
 
     useEffect(() => {
         refreshData()
@@ -20,33 +22,54 @@ export default function PutMethod() {
                 setUserId(resp[0].userId)
                 setTitle(resp[0].title)
                 setBody(resp[0].body)
+                setId(resp[0].id)
             })
         })
     }
 
-    function deleteUser(id){
+    function deleteUser(id) {
         // alert(id)
         fetch(`http://localhost:3004/data/${id}`, {  // or fetch("http://localhost:3004/data/"+id)
             method: 'DELETE'
-        }).then((result)=>{
-            result.json().then((resp)=>{
+        }).then((result) => {
+            result.json().then((resp) => {
                 refreshData()
                 console.log(resp)
             })
-        })            
+        })
     }
 
     function selectUser(id) {
-        console.log(data[id-1])
-        setUserId(data[id-1].userId)
-        setTitle(data[id-1].title)
-        setBody(data[id-1].body)
+        console.log(data[id - 1])
+        setUserId(data[id - 1].userId)
+        setTitle(data[id - 1].title)
+        setBody(data[id - 1].body)
+        setId(data[id-1].id)
+    }
+
+    function updateUser() {
+        // console.log(userId, title, body)
+        let item= {userId, title, body}
+
+        fetch(`http://localhost:3004/data/${id}`, {  // or fetch("http://localhost:3004/data/"+id)
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(item)
+        }).then((result) => {
+            result.json().then((resp) => {
+                refreshData()
+                console.log(resp)
+            })
+        })
     }
 
     return (
         <div>
             <h1>PUT method</h1>
-            <div style={{display: 'flex'}}>
+            <div style={{ display: 'flex' }}>
                 <table border="1">
                     <tr>
                         <th>User ID</th>
@@ -59,16 +82,16 @@ export default function PutMethod() {
                             <td>{data.userId}</td>
                             <td>{data.title}</td>
                             <td>{data.body}</td>
-                            <td><button onClick={()=>deleteUser(data.id)}>Delete</button></td>
-                            <td><button onClick={()=>selectUser(data.id)}>Update</button></td>                            
+                            <td><button onClick={() => deleteUser(data.id)}>Delete</button></td>
+                            <td><button onClick={() => selectUser(data.id)}>Update</button></td>
                         </tr>
                     )}
                 </table>
-                <div style={{margin: '0 3rem'}}>
-                    <input value={userId} type="number" /><br/><br/>
-                    <textarea value={title}  cols="50" /><br/><br/>
-                    <textarea value={body}  cols="50" /><br/><br/>
-                    <button>Update data</button>
+                <div style={{ margin: '0 3rem' }}>
+                    <input value={userId} onChange={(e) => setUserId(e.target.value)} type="number" /><br /><br />
+                    <textarea value={title} onChange={(e) => setTitle(e.target.value)} cols="50" /><br /><br />
+                    <textarea value={body} onChange={(e) => setBody(e.target.value)} cols="50" /><br /><br />
+                    <button onClick={updateUser}>Update data</button>
                 </div>
             </div>
         </div>
