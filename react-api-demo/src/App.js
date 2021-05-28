2ece11b5c0d47603d60d6fbc8fbe5be4a49e0df0
Neelam618 <neelamchavan410@gmail.com>
2021-05-28 10:01:41 +0530

    displayed data from api in table

--- react-api-demo/src/App.js
+++ react-api-demo/src/App.js
@@ -1,8 +1,37 @@
 import './App.css';
+import React, {useEffect, useState} from 'react'
 
 function App() {
+  const [data, setData] =useState([])
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/posts/1/comments").then((result)=>{
+      result.json().then((resp)=>{
+        // console.log("result",resp)
+        setData(resp)
+      })
+    })
+  }, [])
+  console.log(data)
   return (
-   
+   <div>
+     <h1>Get API Call</h1>
+     <table border="1">
+       <tr>
+         <td>PostId</td>
+         <td>ID</td>
+         <td>Name</td>
+         <td>Eamil</td>
+       </tr>
+       {data.map((item)=>
+        <tr>
+          <td>{item.postId}</td>
+          <td>{item.id}</td>
+          <td>{item.name}</td>
+          <td>{item.email}</td>
+        </tr>
+       )}
+     </table>
+   </div>
   );
 }
 
